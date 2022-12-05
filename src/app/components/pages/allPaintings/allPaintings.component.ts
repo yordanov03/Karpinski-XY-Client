dfa58e01e4d5cb31c5cf7ccb32166b724e4f653e
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2022-12-05 11:58:04 +0100

    renamed

---
+++ src/app/components/pages/allPaintings/allPaintings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-allPaintings',
+  templateUrl: './allPaintings.component.html',
+  styleUrls: ['./allPaintings.component.scss']
+})
+export class AllPaintingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
